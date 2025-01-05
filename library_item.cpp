7acaeb59dcd6dfd924c565c525449715375cd114
tim569 <timothyobrike@gmail.com>
2025-01-05 21:42:18 +0100

    Updated library item

---
+++ library_item.cpp
@@ -0,0 +1,46 @@
+#include "library_item.h"
+
+ LibraryItem::LibraryItem(const std::string& title, bool& isCheckedOut , const int& dueDate)
+ {
+    this->title = title;
+    this->isCheckedOut = isCheckedOut;
+    this->dueDate = dueDate;
+ }
+
+    //Getters ans Setters
+std::string LibraryItem::getTitle()const{
+    return this->title;
+}
+    
+bool LibraryItem::getIsCheckedOut()const{
+    return this->isCheckedOut;
+}
+
+void LibraryItem::setTitle(const std::string& newTitle){
+    this->title = newTitle;
+}
+
+
+// Real functions
+    
+void LibraryItem::checkOut(const int& dueDate){
+    this->isCheckedOut = true;
+    this->dueDate = dueDate;
+
+}
+
+void LibraryItem::returnItem(){
+    this->isCheckedOut = false;
+    this->dueDate = 0;
+}
+
+// Function to Extend the due date for an item.
+int LibraryItem::renewItem(int extraDays){
+    this->dueDate += extraDays;
+    return this->dueDate;
+}
+    
+// Function to Mark an item as lost and update its status.
+void LibraryItem::markAsLost(){
+    this->islost = true;
+}
--- library_item.h
+++ library_item.h
@@ -12,34 +12,25 @@
 //  ● Use encapsulation to protect data members.
 #ifndef LIBRARY_ITEM_H
 #define LIBRARY_ITEM_H
+#include <string>
 class LibraryItem {
 private:
     std::string title;
     bool isCheckedOut;
-    std::string dueDate;
+    int dueDate;
+    bool islost = false;
+
 public:
-    //Constuctor
-    LibraryItem(const std::string& title, bool& isCheckedOut , const std::string& dueDate);
+    LibraryItem(const std::string& title, bool& isCheckedOut , const int& dueDate);
     //Getters ans Setters
-    std::string getTitle()const{
-        return Title;
-    }
-    void setTitle(const std::string& newTitle){
-        title=newTitle;
-    }
-    bool getIsCheckedOut()const{
-        return isCheckedOutr;
-    }
-    void IsCheckedOut(bool newIsCheckedOut){
-        isCheckedOut=newIsCheckedOut;
-    }
-    std::string DueDate()const{
-        return  dueDate;
-    }
-    void setDueDate(const std::string& newDueDate){
-        dueDate=newDueDate;
-    }
-    void checkOut(const std::string& dueDate);
+    std::string getTitle()const;
+    bool getIsCheckedOut()const;
+    void setTitle(const std::string& newTitle);
+
+
+
+
+    void checkOut(const int& dueDate);
     void returnItem();
     // Function to Extend the due date for an item.
     int renewItem(int extraDays);
